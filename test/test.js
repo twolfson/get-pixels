5db6cd2d9af06fac1ac5e138bf820596599ba71e
Todd Wolfson <todd@twolfson.com>
2014-10-19 02:42:41 -0700

    Starting to look into getting browser tests passing

--- test/test.js
+++ test/test.js
@@ -31,7 +31,7 @@ function test_image(t, img) {
   t.equals(img.get(0, 2, 0), 0)
   t.equals(img.get(0, 2, 1), 0)
   t.equals(img.get(0, 2, 2), 0xff)
-  
+console.warn('waat');
   for(var i=4; i<8; ++i) {
     for(var j=0; j<16; ++j) {
       t.equals(img.get(j, i, 0), 0xff)
@@ -41,111 +41,105 @@ function test_image(t, img) {
   }
 }
 
-test("get-pixels", function(t) {
-  getPixels(path.join(__dirname, "lena.png"), function(err, pixels) {
-    if(err) {
-      t.assert(false)
-    } else {
-      t.equals(pixels.shape.join(","), "512,512,4")
-    }
-    t.end()
-  })
-})
+// test("get-pixels", function(t) {
+//   getPixels(path.join(__dirname, "lena.png"), function(err, pixels) {
+//     if(err) {
+//       t.assert(false)
+//     } else {
+//       t.equals(pixels.shape.join(","), "512,512,4")
+//     }
+//     t.end()
+//   })
+// })
 
-test("get-pixels-png", function(t) {
-  getPixels(path.join(__dirname, "test_pattern.png"), function(err, pixels) {
-    if(err) {
-      t.error(err, "failed to parse png")
-      t.end()
-      return
-    }
-    test_image(t, pixels)
-    t.end()
-  })
-})
+// test("get-pixels-png", function(t) {
+//   getPixels(path.join(__dirname, "test_pattern.png"), function(err, pixels) {
+//     if(err) {
+//       t.error(err, "failed to parse png")
+//       t.end()
+//       return
+//     }
+//     test_image(t, pixels)
+//     t.end()
+//   })
+// })
 
-/*
-test("get-pixels-ppm", function(t) {
-  getPixels(path.join(__dirname, "test_pattern.ppm"), function(err, pixels) {
-    if(err) {
-      t.error(err, "failed to parse ppm")
-      t.end()
-      return
-    }
-    test_image(t, pixels)
-    t.end()
-  })
-})
-*/
+// /*
+// test("get-pixels-ppm", function(t) {
+//   getPixels(path.join(__dirname, "test_pattern.ppm"), function(err, pixels) {
+//     if(err) {
+//       t.error(err, "failed to parse ppm")
+//       t.end()
+//       return
+//     }
+//     test_image(t, pixels)
+//     t.end()
+//   })
+// })
+// */
 
-test("get-pixels-jpg", function(t) {
-  //FIXME: This fails because JPEG is a lossy format
-  getPixels(path.join(__dirname, "test_pattern.jpg"), function(err, pixels) {
-    if(err) {
-      t.error(err, "failed to parse jpg")
-      t.end()
-      return
-    }
-    test_image(t, pixels)
-    t.end()
-  })
-})
+// // test("get-pixels-jpg", function(t) {
+// //   //FIXME: This fails because JPEG is a lossy format
+// //   getPixels(path.join(__dirname, "test_pattern.jpg"), function(err, pixels) {
+// //     if(err) {
+// //       t.error(err, "failed to parse jpg")
+// //       t.end()
+// //       return
+// //     }
+// //     test_image(t, pixels)
+// //     t.end()
+// //   })
+// // })
 
-test("get-pixels-gif", function(t) {
-  getPixels(path.join(__dirname, "test_pattern.gif"), function(err, pixels) {
-    if(err) {
-      t.error(err, "failed to parse gif")
-      t.end()
-      return
-    }
-    test_image(t, pixels.pick(0))
-    t.end()
-  })
-})
+// test("get-pixels-gif", function(t) {
+//   getPixels(path.join(__dirname, "test_pattern.gif"), function(err, pixels) {
+//     if(err) {
+//       t.error(err, "failed to parse gif")
+//       t.end()
+//       return
+//     }
+//     test_image(t, pixels.pick(0))
+//     t.end()
+//   })
+// })
 
-/*
-test("get-pixels-bmp", function(t) {
-  getPixels(path.join(__dirname, "test_pattern.bmp"), function(err, pixels) {
-    if(err) {
-      t.error(err, "failed to parse bmp")
-      t.end()
-      return
-    }
-    test_image(t, pixels)
-    t.end()
-  })
-})
-*/
+// /*
+// test("get-pixels-bmp", function(t) {
+//   getPixels(path.join(__dirname, "test_pattern.bmp"), function(err, pixels) {
+//     if(err) {
+//       t.error(err, "failed to parse bmp")
+//       t.end()
+//       return
+//     }
+//     test_image(t, pixels)
+//     t.end()
+//   })
+// })
+// */
 
-test("data url", function(t) {
-  var url = "data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7"
-  getPixels(url, function(err, data) {
-    if(err) {
-      console.log(err)
-      t.error("failed to read data url")
-      t.end()
-      return
-    }
-    t.ok(true, 'data url opened without crashing')
-    t.end()
-  })
-})
+// test("data url", function(t) {
+//   var url = "data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7"
+//   getPixels(url, function(err, data) {
+//     if(err) {
+//       console.log(err)
+//       t.error("failed to read data url")
+//       t.end()
+//       return
+//     }
+//     t.ok(true, 'data url opened without crashing')
+//     t.end()
+//   })
+// })
 
 test("get-pixels-buffer", function(t) {
-  fs.readFile(path.join(__dirname, "test_pattern.png"), function(err, buffer) {
+  var buffer = fs.readFileSync(path.join(__dirname, "test_pattern.png"));
+  getPixels(buffer, "image/png", function(err, pixels) {
     if(err) {
-      t.error(err, "failed to read file")
+      t.error(err, "failed to parse buffer")
       t.end()
       return
     }
-    getPixels(buffer, "image/png", function(err, pixels) {
-      if(err) {
-        t.error(err, "failed to parse buffer")
-        t.end()
-        return
-      }
-      test_image(t, pixels)
-      t.end()
-    })
+    test_image(t, pixels)
+    t.end()
   })
 })
